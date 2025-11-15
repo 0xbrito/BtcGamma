579b212d8bba855eefb35c708d0c235ceb4bbf86
Diego Brito <84880186+0xbrito@users.noreply.github.com>
2025-11-15 14:38:50 -0300

    ✨ setup remappings for vscode

--- src/BtcGammaStrategy.sol
+++ src/BtcGammaStrategy.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import {ERC4626} from "@solady-tokens/ERC4626.sol";
 
-contract BtcGammaStrategy {
+contract BtcGammaStrategy is ERC4626 {
     constructor() public {}
+
+    function _convertToShares(
+        uint256 assets
+    ) internal pure override returns (uint256) {
+        return assets;
+    }
 }
